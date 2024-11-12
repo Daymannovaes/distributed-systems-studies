e91455de154f88e7c049965c2b7d9c8e181bc2cd
Dayman Novaes <daymannovaes@gmail.com>
2024-11-12 17:30:52 -0300

    change folder root structure

---
+++ golang-tests/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello", Sum(1, 2))
+}
---
+++ golang-tests/sum.go
@@ -0,0 +1,9 @@
+package main
+
+func Sum(a int, b int) int {
+	return a + b
+}
+
+func Concat(a, b string) (string, string) {
+	return a, b
+}
